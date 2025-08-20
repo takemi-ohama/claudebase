5005ef0ee3f7f9f8a29d3cfcfe09ef71db27db34
takemi.ohama <takemi.ohama@gmail.com>
2025-08-21 06:53:15 +0900

    .mcp.json更新

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export U_ID=$(id -u)
 export G_ID=$(id -g)
 export DOCKER_GID=$(grep docker /etc/group | cut -d: -f3)
+export COMPOSE_PROJECT_NAME=$(basename "$PWD")
 docker compose build
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 export U_ID=$(id -u)
 export G_ID=$(id -g)
 export DOCKER_GID=$(grep docker /etc/group | cut -d: -f3)
+export COMPOSE_PROJECT_NAME=$(basename "$PWD")
 docker compose stop -t0
 docker compose up -d --scale dev=3
